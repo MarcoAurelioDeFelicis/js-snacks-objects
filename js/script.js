206c084c7f5d6b914069f89de4a366778d4ade4a
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-25 14:20:05 +0100

    ADD: snack 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,87 @@
 console.log ('JS Snacks Object')
+
+    // Snack 1
+//Crea un array composto da 10 automobili. Ogni oggetto automobile avrà le seguenti proprietà: marca,
+//alimentazione (benzina,
+//diesel, gpl, elettrico, metano).
+//modello e Dividi le automobili in 3 array separati: nel primo array solo le auto a
+//benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+//Infine stampa separatamente i 3 array.
+
+
+const cars = [
+    {
+     marca : 'Fiat', 
+     modello: 'Punto',
+     alimentazione: 'Benzina'
+    },
+    {
+     marca : 'Ford', 
+     modello: 'Fiesta',
+     alimentazione: 'Metano'
+    },
+    {
+     marca : 'Alfa Romeo', 
+     modello: 'Giulietta',
+     alimentazione: 'gpl'
+    },
+    {
+     marca : 'Toyota', 
+     modello: 'Auris',
+     alimentazione: 'Elettrica'
+    },
+    {
+     marca : 'Ford', 
+     modello: 'Fiesta elettrica',
+     alimentazione: 'Metano'
+    },
+    {
+     marca : 'Lancia', 
+     modello: 'Delta',
+     alimentazione: 'Benzina'
+    },
+    {
+     marca : 'Alfa Romeo', 
+     modello: '159',
+     alimentazione: 'Diesel'
+    },
+    {
+     marca : 'Mercedez', 
+     modello: 'smart',
+     alimentazione: 'Diesel'
+    },
+    {
+     marca : 'BMW', 
+     modello: 'X1',
+     alimentazione: 'Ibrida'
+    },
+    {
+     marca : 'Fiat', 
+     modello: 'Punto',
+     alimentazione: 'gpl'
+    }
+]
+
+const benzina = []
+const diesel = []
+const leAltre = []
+
+for (let i = 0; i < cars.length; i++) {
+    const car = cars[i]
+
+
+    if (car.alimentazione === 'Benzina') {
+        benzina.push(car)
+        
+    }else if (cars.alimentazione === 'Diesel') {
+        diesel.push(car)
+
+    }else {
+        leAltre.push(car)
+    }
+    
+}
+
+console.log(benzina)
+console.log(diesel)
+console.log(leAltre)
