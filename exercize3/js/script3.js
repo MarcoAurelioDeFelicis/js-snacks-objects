e497e1db53897d935a530e299137ee26fb14a0ef
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-25 16:16:01 +0100

    ADD: snack 3

---
+++ exercize3/js/script3.js
@@ -0,0 +1,45 @@
+console.log ('JS Snacks Object')
+
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Crea un nuovo array con la lista dei mammiferi.
+
+const animali = [
+    {
+     nome: 'Bradipo',
+     famiglia: 'Bradypodidae',
+     classe: 'Mammifero'
+    },
+    {
+     nome: 'Panada',
+     famiglia: 'Orsi',
+     classe: 'Mammifero'
+    },
+    {
+     nome: 'Cmaleonte',
+     famiglia: 'Lucertole',
+     classe: 'Rettili'
+    },
+    {
+     nome: 'Tardigrado',
+     famiglia: 'celomati',
+     classe: 'Protostomia'
+    },
+    {
+     nome: 'orca',
+     famiglia: 'delfini',
+     classe: 'Mammifero'
+    },
+
+]
+
+const mammiferi = []
+
+for (let i = 0; i < animali.length; i++) {
+    const specie = animali[i]
+
+
+    if (specie.classe === 'Mammifero') {
+        mammiferi.push(specie) }
+}
+console.log (mammiferi) 
